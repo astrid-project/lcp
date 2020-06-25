ecc20240cf5f92d59a53502d9d507ff41745a280
Alex Carrega <contact@alexcarrega.com>
2020-06-25 13:56:07 +0200

    Minor changes

--- reader/arg.py
+++ reader/arg.py
@@ -10,50 +10,65 @@ __all__ = [
 
 class Arg_Reader:
     db = None
+    cr = None
+    ap = None
 
     @classmethod
-    def read(cls):
-        cr = Config_Reader()
-
-        ap = Argument_Parser(prog='python3 main.py',
-                            description=f'{cr.title}: {cr.description}')
-        add = ap.add_argument
+    def init(cls):
+        cls.cr = Config_Reader()
+        cls.cr.read()
+        cls.ap = Argument_Parser(prog='python3 main.py',
+                                 description=f'{cls.cr.title}: {cls.cr.description}')
+        add = cls.ap.add_argument
 
         add('--host', '-o', type=str,
-            help='Hostname/IP of the REST Server', default=cr.lcp_host)
+            help='Hostname/IP of the REST Server', default=cls.cr.cb_host)
         add('--port', '-p', type=int,
-            help='TCP Port of the REST Server', default=cr.lcp_port)
+            help='Tcr Port of the REST Server', default=cls.cr.cb_port)
 
-        add('--auth-max-ttl', '-t', type=str,
-            help='Max authentication db TTL', default=cr.auth_max_ttl)
+        add('--hb-timeout', '-b', type=str,
+            help='Timeout (with unit, e.g.: 10s) for heartbeat with LCP', default=cls.cr.hb_timeout)
+        add('--hb-period', '-r', type=str,
+            help='Period (with unit, e.g.: 1min) for the heartbeat with the LCP', default=cls.cr.hb_period)
+        add('--hb-auth-expiration', '-x', type=str,
+            help='Period (with unit, e.g.: 1min) for auth expiration used in the heartbeat with the LCP',
+            default=cls.cr.hb_auth_expiration)
 
-        add('--polycube-host', '-s', type=str,
-            help='Hostname/IP of Polycube', default=cr.polycube_host)
-        add('--polycube-port', '-c', type=int,
-            help='Port of Polycube', default=cr.polycube_port)
-        add('--polycube-timeout', '-m', type=str, help='Timeout for Polycube connection',
-            default=cr.polycube_timeout)
+        add('--es-endpoint', '-e', type=str,
+            help='Elasticsearch server hostname/IP:port',
+            default=cls.cr.es_endpoint)
+        add('--es-timeout', '-s', type=str,
+            help='Timeout (with unit, e.g.: 10s) for the connection to Elasticsearch',
+            default=cls.cr.es_timeout)
+        add('--es-retry_period', '-y', type=str,
+            help='Period (with unit, e.g.: 1min) to retry the connection to Elasticsearch',
+            default=cls.cr.es_retry_period)
 
         add('--dev-username', '-u', type=str,
-            help='Authorized username', default=cr.dev_username)
+            help='Authorized username', default=cls.cr.dev_username)
         add('--dev-password', '-a', type=str,
-            help='Authorized password', default=cr.dev_password)
-
+            help='Authorized password', default=cls.cr.dev_password)
         add('--log-level', '-l', choices=Log.get_levels(),
-            help='Log level', default=cr.log_level)
+            help='Log level', default=cls.cr.log_level)
 
         add('--write-config', '-w',
             help='Write options to config.ini', action='store_true')
         add('--version', '-v', help='Show version',
-            action='store_const', const=cr.version)
+            action='store_const', const=cls.cr.version)
 
-        cls.db = ap.parse_args()
-        cls.db.config = cr
+        return cls.ap
 
-        for field in ('auth_max_ttl', 'polycube_timeout'):
+    @classmethod
+    def read(cls):
+        cls.init()
+
+        cls.db = cls.ap.parse_args()
+        cls.db.config = cls.cr
+        for field in ('hb_timeout', 'hb_period', 'hb_auth_expiration',
+                      'es_timeout', 'es_retry_period'):
             setattr(cls.db, field, get_seconds(getattr(cls.db, field)))
 
         if cls.db.write_config:
-            cr.write(cls.db)
+            cls.cr.write(cls.db)
 
         return cls.db
