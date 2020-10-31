2446225ada45ac5a552c541db353cecb1fb5bfbc
Alex Carrega <contact@alexcarrega.com>
2020-10-31 17:38:22 +0100

    Minor changes

--- about.py
+++ about.py
@@ -1,3 +1,3 @@
 title = 'Local Control Plane'
 description = 'In each local agent, the control plane is responsible for programmability, i.e., changing the behaviour of the data plane at run-time.'
-version = '1.1.0'
+version = '1.0.1'
