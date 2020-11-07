4d33382bfc33a5e7bc1291ca096f55b665be0fb7
Alex Carrega <contact@alexcarrega.com>
2020-11-07 23:36:14 +0100

    Major fix

--- main.py
+++ main.py
@@ -5,17 +5,10 @@ path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
 os.chdir(dir_path)
 
-while True:
-    from utils.exception import reload_import
-    try:
-        from about import title, version
-        from api import api
-        from reader.arg import Arg_Reader
-        from werkzeug.serving import run_with_reloader
-        import waitress
-        break
-    except Import_Error as error:
-        reload_import(error)
+from about import title, version
+from api import api
+from reader.arg import Arg_Reader
+import waitress
 
 db = Arg_Reader.read()
 
@@ -25,11 +18,7 @@ print(f'{title} version:{version}')
 if db.version is not None:
     print(db.version)
 else:
-    @run_with_reloader
-    def run_server():
-        u, p = db.dev_username, db.dev_password
-        waitress.serve(api(title=title, version=version,
-                           dev_username=u, dev_password=p),
-                       host=db.host, port=db.port, expose_tracebacks=False)
-
-    run_server()
+    u, p = db.dev_username, db.dev_password
+    waitress.serve(api(title=title, version=version,
+                        dev_username=u, dev_password=p),
+                   host=db.host, port=db.port, expose_tracebacks=False)
--- utils/exception.py
+++ utils/exception.py
@@ -5,7 +5,6 @@ import sys
 
 __all__ = [
     'extract_info',
-    'reload_import',
     'to_str'
 ]
 
@@ -18,20 +17,6 @@ def extract_info(exception):
     except Exception:
         reason = str(exception)
     return dict(reason=reason, filename=filename, line=exc_tb.tb_lineno)
-
-
-def reload_import(error):
-    error_data = str(error).split()
-    if ['cannot', 'import'] == error_data[0:2]:
-        name = error_data[2]
-        path = Path(__file__).parent / '../requirements.txt'
-        with path.open('r') as req_file:
-            for mod in req_file:
-                if mod == name.replace('_', '-'):
-                    os.system('pip3 install -r requirements.txt')
-                    break
-    print(f'Error: {error}')
-    sys.exit(1)
 
 
 def to_str(exception):
