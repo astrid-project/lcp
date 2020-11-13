f0078c87eeb12ef0b8bbb980490d1e5d884be782
Alex Carrega <contact@alexcarrega.com>
2020-11-13 11:25:10 +0100

    Minor fix

--- utils/schema.py
+++ utils/schema.py
@@ -1,4 +1,5 @@
 from marshmallow.fields import List
+from utils.sequence import wrap
 
 __all__ = [
     'List_or_One'
@@ -9,7 +10,7 @@ class List_or_One(List):
     def _deserialize(self, value, attr, data, **kwargs):
         if value:
             if isinstance(value, str):
-                value = [value]
-            elif isinstance(value, self.inner):
                 value = value.replace('\r', '').splitlines();
-        return super(List_or_One, self)._deserialize(value, attr, data)
+            else:
+                value = wrap(value)
+        return super(List_or_One, self)._deserialize(value, attr, data, **kwargs)
