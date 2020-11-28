4727ee6b68eb032c551d9a6cde9dc53c9d96264d
Alex Carrega <contact@alexcarrega.com>
2020-11-28 11:51:00 +0100

    Server ident upate

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
 os.chdir(dir_path)
 
-from about import title, version
+from about import project, title, version
 from api import api
 from reader.arg import Arg_Reader
 import waitress
 
 db = Arg_Reader.read()
 
-print(f'{title} version:{version}')
-
+ident = f'{project} - {title} v:{version}'
+print(ident)
 
 if db.version is not None:
     print(db.version)
 else:
     u, p = db.dev_username, db.dev_password
-    waitress.serve(api(title=title, version=version,
+    waitress.serve(api(title=title, version=version, ident=ident,
                         dev_username=u, dev_password=p),
                    host=db.host, port=db.port, expose_tracebacks=False)
