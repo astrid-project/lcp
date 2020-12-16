7b34601b6bd3280bc8419ab408085fc28d65761e
Alex Carrega <contact@alexcarrega.com>
2020-12-16 17:18:57 +0100

    Docs theme update

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinx_theme_pd
+import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -58,8 +58,7 @@ exclude_patterns = []
 
 html_logo = '../logo.png'
 html_favicon = '../logo.ico'
-html_theme = 'sphinx_theme_pd'
-html_theme_path = [sphinx_theme_pd.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 issues_github_path = "astrid-project/lcp"
 
